80559d88770b16cbc5fe55b5e820af5ea53f156f
paul <423357+toothbrush@users.noreply.github.com>
2021-05-06 20:02:21 +1000

    nix: Make pandoc available.

---
+++ shell.nix
@@ -0,0 +1,9 @@
+# Use latest stable Nix channel
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-20.09.tar.gz") { } }:
+
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = [
+    pkgs.pandoc
+  ];
+}
