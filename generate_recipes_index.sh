0c270e2ff5a334ecbe941d77486e93de7a595f74
paul <423357+toothbrush@users.noreply.github.com>
2021-05-06 22:18:16 +1000

    Fix index generation: grab tags, too.

--- generate_recipes_index.sh
+++ generate_recipes_index.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+_get_field() {
+    local field=$1
+    local filename=$2
+    grep -E "^${field}:" "${filename}" | head -n 5 | sed "s/^${field}: //"
+}
+
 cat <<FRONT
 ---
 title: List of Recipes
@@ -11,9 +17,9 @@ FRONT
 
 for recipe in recipes/*.md; do
     filename=$(basename ${recipe})
-    title=$(grep -E "^title:" "${recipe}" | sed 's/^title: //')
+    title=$(_get_field title "${recipe}")
     url="/recipes/${filename%.md}.html"
-    tags="main, vegan"
+    tags=$(_get_field tags "${recipe}")
 
     echo "* [${title}](${url}). ${tags}"
 done | sort
