1a9ca898f6ea4c921c7c620ca843ca72a7b31c71
paul <423357+toothbrush@users.noreply.github.com>
2021-05-06 23:29:48 +1000

    Generate sections in recipes.md by tag.

--- lib/generate_recipes_index.sh
+++ lib/generate_recipes_index.sh
@@ -13,11 +13,22 @@ title: List of Recipes
 
 FRONT
 
-for recipe in recipes/*.md; do
-    filename=$(basename "${recipe}")
-    title=$(_get_field title "${recipe}")
-    url="/recipes/${filename%.md}.html"
-    tags=$(_get_field tags "${recipe}")
+echo "Collecting recipe tags list..." >&2
+readarray -t tags < <( "${SCRIPT_DIR}/tag_list.sh" )
 
-    echo "* [${title}](${url}). ${tags}"
-done | sort
+for tag in "${tags[@]}"; do
+    echo "Composing ${tag} section..." >&2
+    echo
+    echo "## $(_title_case "${tag}")"
+    echo
+    readarray -t matched < <( "${SCRIPT_DIR}/recipes_for_tag.sh" "${tag}" )
+
+    for recipe in "${matched[@]}"; do
+        filename=$(basename "${recipe}")
+        title=$(_get_field title "${recipe}")
+        url="/recipes/${filename%.md}.html"
+        recipe_tags=$(_get_field tags "${recipe}")
+
+        echo "* [${title}](${url}). ${recipe_tags}"
+    done | sort
+done
