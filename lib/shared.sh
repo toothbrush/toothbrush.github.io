5ef05ef98d084541993ef36be96577cd45c0915b
paul <423357+toothbrush@users.noreply.github.com>
2021-06-09 19:47:28 +1000

    lib/shared.sh: Show sensible error if Bash < 5.

--- lib/shared.sh
+++ lib/shared.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+if ((BASH_VERSINFO[0] < 5))
+then
+  echo "Sorry, you need at least bash-5.0 to run this script." >&2
+  exit 1
+fi
+
 _title_case() {
     printf "%s" "$1" | sed 's/.*/\L&/; s/[a-z]*/\u&/g'
 }
